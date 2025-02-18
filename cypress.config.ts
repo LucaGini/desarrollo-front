a18d8495f5164712def3cf57cdacf1bd8e15f586
Simon-PF2003 <ipcsimon.tel@gmail.com>
2025-02-17 22:30:42 -0300

    Conexion back? 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://desarrollo-front-indol.vercel.app/', // URL donde corre Angular
+    baseUrl: 'http://localhost:4200', // URL donde corre Angular
     supportFile: false
   }
 });
